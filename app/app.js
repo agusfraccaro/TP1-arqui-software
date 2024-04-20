b8e849d26afa4bad2b1faf386a302fb856d61af3
Emanuel Pelozo <emanuel.pelozo@outlook.com>
2024-04-20 12:43:23 -0300

    modify gitignore and some format refactors

--- app/app.js
+++ app/app.js
@@ -9,11 +9,11 @@ const id = nanoid();
 app.use((req, res, next) => {
     res.setHeader('X-API-Id', id);
     next();
-})
+});
 
 app.get('/ping', (req, res) => {
     res.status(200).send('pong');
-})
+});
 
 app.get('/dictionary', async (req, res) => {
     const {word} = req.query;
@@ -21,45 +21,45 @@ app.get('/dictionary', async (req, res) => {
         return res.status(400).send('Missing word query param');
     }
 
-    try{
+    try {
         const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
 
         const { phonetics, meanings } = response.data[0];
             
         res.status(200).send({ phonetics, meanings });
-    }catch(error){
+    } catch(error){
         res.status(500).send('Error');
     }
-})
+});
 
 app.get('/spaceflight_news', async (req, res) => {
-    try{
+    try {
         const response = await axios.get('https://api.spaceflightnewsapi.net/v4/articles?limit=5');
         let titles = [];
 
         response.data.results.forEach(article => {
-            if(article.hasOwnProperty('title')){
-                titles.push(article.title)
+            if (article.hasOwnProperty('title')) {
+                titles.push(article.title);
             }
         });
 
         res.status(200).send(titles);
-    }catch(error){
+    } catch(error){
         res.status(500).send('Error');
     }
     
-})
+});
 
 app.get('/quote', async (req, res) => {
-    try{
+    try {
         const response = await axios.get('https://api.quotable.io/random');
 
         const { content, author } = response.data;
             
         res.status(200).send({ content, author });
-    }catch(error){
+    } catch(error){
         res.status(500).send('Error');
     }
-})
+});
 
-app.listen(3000, console.log("Escuchando en puerto 3000"))
+app.listen(3000, console.log("Escuchando en puerto 3000"));
