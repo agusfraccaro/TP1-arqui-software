6e54b655f6eb3967fc6be3dd270fc21f446ac016
Emanuel Pelozo <emanuel.pelozo@outlook.com>
2024-04-28 04:01:56 -0300

    add more metrics, change error status codes, test replication

--- app/app.js
+++ app/app.js
@@ -5,7 +5,10 @@ import redis from 'redis';
 import { Metrics } from './metrics.js';
 
 //Const
-
+const errorMessages = {
+    dependencyError: 'Error communicating with the dependency API.',
+    internalError: 'Internal Server Error.'
+};
 
 // Init
 const app = express();
@@ -67,8 +70,7 @@ app.get('/dictionary', async (req, res) => {
 
         res.status(200).send({ phonetics, meanings });
     } catch(error){
-        console.log(error)
-        res.status(500).send(`Error ${error}`);
+        handleError(res, error);
     }
 });
 
@@ -94,7 +96,7 @@ app.get('/spaceflight_news', async (req, res) => {
 
         res.status(200).send(titles);
     } catch(error){
-        res.status(500).send('Error');
+        handleError(res, error);
     }
     
 });
@@ -111,7 +113,7 @@ app.get('/quote', async (req, res) => {
 
         res.status(200).send({ content, author });
     } catch(error){
-        res.status(500).send('Error');
+        handleError(res, error);
     }
 });
 
@@ -128,4 +130,20 @@ function getDataFromRedis(key) {
     });
 }
 
-app.listen(3000, console.log("Escuchando en puerto 3000"));
+function handleError(res, error) {
+    console.log(error);
+    if (error.response) {
+        console.log(`HTTP Error: ${error.response.status} - ${error.response.statusText}`);
+        console.log('Response Data:', error.response.data);
+        res.status(error.response.status).send(error.response.data);
+    } else if (error.request) {
+        // 424 failed dependency
+        console.log('Failed dependency error:', error.request);
+        res.status(424).send(errorMessages.dependencyError);
+    } else {
+        console.log('Internal Error:', error.message);
+        res.status(500).send(errorMessages.internalError);
+    }
+}
+
+app.listen(3000, console.log("Listening on port 3000"));
